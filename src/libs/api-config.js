8a7f648cb1bbf99b96f121216a35c1afa2df9f13
CHANN <chann@chann.kr>
2018-08-06 05:06:31 +0900

    Temp: Change Endpoint to Junglenet

--- src/libs/api-config.js
+++ src/libs/api-config.js
@@ -6,7 +6,7 @@ export const apiVersion = 'v1'
 const hostname = window && window.location && window.location.hostname
 if (hostname === 'eosocial.owdin.network') {
   // baseEndpoint = 'https://eos.owdin.network:9999'
-  baseEndpoint = ''
+  baseEndpoint = 'http://jungle.cryptolions.io:18888/v1/chain/get_info'
   account = process.env.ACCOUNT
 // } else if (hostname === 'dev.eosocial.app') {
 //   baseEndpoint = 'https://dev.eos.owdin.network:9999'
