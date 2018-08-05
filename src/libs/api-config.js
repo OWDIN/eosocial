01f61c6626bfd60443dddd177c794c4509fdcdc5
CHANN <chann@chann.kr>
2018-08-06 05:12:55 +0900

    Update: Configuration - api-config.js

--- src/libs/api-config.js
+++ src/libs/api-config.js
@@ -5,14 +5,11 @@ export const apiVersion = 'v1'
 
 const hostname = window && window.location && window.location.hostname
 if (hostname === 'eosocial.owdin.network') {
-  // baseEndpoint = 'https://eos.owdin.network:9999'
-  baseEndpoint = 'http://jungle.cryptolions.io:18888/v1/chain/get_info'
-  account = process.env.ACCOUNT
-// } else if (hostname === 'dev.eosocial.app') {
-//   baseEndpoint = 'https://dev.eos.owdin.network:9999'
+  baseEndpoint = process.env.CHAIN_ENDPOINT || 'http://jungle.cryptolions.io:18888'
+  account = process.env.ACCOUNT || 'eossocialapp'
 } else {
-  baseEndpoint = process.env.REACT_APP_CHAIN_ENDPOINT || 'http://localhost:8888'
-  account = process.env.DAPP_ACCOUNT || 'eossocialapp'
+  baseEndpoint = process.env.CHAIN_ENDPOINT || 'http://localhost:8888'
+  account = process.env.ACCOUNT || 'eossocialapp'
 }
 export const ENDPOINT = baseEndpoint
 
@@ -20,4 +17,5 @@ export const CHAIN_ID = process.env.CHAIN_ID || ''
 export const DAPP_ACCOUNT = account
 export const DAPP_PRIVATE_KEY = process.env.ACTIVE_PRIVATE_KEY
 
+console.table(process.env)
 export default ENDPOINT
