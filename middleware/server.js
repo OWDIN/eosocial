da49c51635a48c55dee5f326432746ddbcbfcc86
CHANN <chann@chann.kr>
2018-08-07 01:48:54 +0900

    Init: Environment Config for Creating Account (#1)

--- middleware/server.js
+++ middleware/server.js
@@ -1,16 +1,46 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const PORT = process.env.MIDDLEWARE_PORT || 5000
 
-const DAPP_ACCOUNT = process.env.ACCOUNT || 'eossocialapp'
+let baseEndpoint = ''
+let account = ''
+
+const hostname = process.env.HOSTNAME || 'localhost'
+if (hostname === 'eosocial.owdin.network') {
+  baseEndpoint = process.env.CHAIN_ENDPOINT || 'https://api.jungle.alohaeos.com'
+  account = process.env.ACCOUNT || 'eossocialapp'
+} else {
+  baseEndpoint = process.env.CHAIN_ENDPOINT || 'http://localhost:8888'
+  account = process.env.ACCOUNT || 'eossocialapp'
+}
+const ENDPOINT = baseEndpoint
+
+const PORT = process.env.MIDDLEWARE_PORT || 5000
+const CHAIN_ID = process.env.CHAIN_ID || ''
+const DAPP_ACCOUNT = account
 const DAPP_PRIVATE_KEY = process.env.ACTIVE_PRIVATE_KEY || '<YOUR-DAPP-PRIVATE-KEY-FOR-TEST>'
 const DEFAULT_SYMBOL = process.env.DEFAULT_SYMBOL || 'SYS'
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' })
+const config = {
+  chainId: CHAIN_ID,
+  keyProvider: [
+    DAPP_PRIVATE_KEY,
+  ],
+  httpEndpoint: ENDPOINT,
+  expireInSeconds: 60,
+  broadcast: true,
+  verbose: false, // true in dev
+  sign: true,
+}
+
+app.get('/v1/health', (req, res) => {
+  res.send({
+    code: 200,
+    status: 'running',
+  })
 })
 
+// render react
 if (process.env.NODE_ENV === 'production') {
   // serve react static files
   app.use(express.static(path.join(__dirname, '../build')))
